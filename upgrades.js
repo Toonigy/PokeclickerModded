c3f381741d42fbdfe0844c7df5824cbc2d830b24
Toonigy <62924215+Toonigy@users.noreply.github.com>
2024-05-10 21:36:19 -0700

    Free upgrades

--- upgrades.js
+++ upgrades.js
@@ -32,31 +32,31 @@ var addUpgrade = function(name,cost,type,amount,require,flavorText, requiredUpgr
 //	catchTime: 3000,
 //									name,cost,type,amount,require,flavorText
 var initUpgrades = function(){
-	addUpgrade("Pokeball upgrade I",1000, "catchBonus",5,0,"New technology gives you a 5% bonus to catch rate",null); 
-	addUpgrade("Pokeball upgrade II",5000, "catchBonus",5,3,"New technology gives you a 5% bonus to catch rate",0); 
-	addUpgrade("Pokeball upgrade III",10000, "catchBonus",5,5,"New technology gives you a 5% bonus to catch rate",1); 
-	addUpgrade("Pokeball upgrade IV",25000, "catchBonus",5,7,"New technology gives you a 5% bonus to catch rate",2); 
-	addUpgrade("Pokeball upgrade V",75000, "catchBonus",10,10,"New technology gives you a 10% bonus to catch rate",3); 
-	addUpgrade("Experience upgrade I",500, "expMultiplier",0.10,0,"New technology gives you a 10% bonus to experience gain",null); 
-	addUpgrade("Experience upgrade II",5000, "expMultiplier",0.10,1,"New technology gives you a 10% bonus to experience gain",5); 
-	addUpgrade("Experience upgrade III",50000, "expMultiplier",0.10,4,"New technology gives you a 10% bonus to experience gain",6); 
-	addUpgrade("Experience upgrade IV",100000, "expMultiplier",0.10,6,"New technology gives you a 10% bonus to experience gain",7); 
-	addUpgrade("Experience upgrade V",500000, "expMultiplier",0.20,12,"New technology gives you a 20% bonus to experience gain",8); 
-	addUpgrade("Catch time upgrade I",2000, "catchTime",250,0,"Decrease the catch time by 250 milliseconds",null); 
-	addUpgrade("Catch time upgrade II",10000, "catchTime",250,2,"Decrease the catch time by 250 milliseconds",10); 
-	addUpgrade("Catch time upgrade III",25000, "catchTime",250,5,"Decrease the catch time by 250 milliseconds",11); 
-	addUpgrade("Catch time upgrade IV",75000, "catchTime",250,8,"Decrease the catch time by 250 milliseconds",12); 
-	addUpgrade("Catch time upgrade V",1000000, "catchTime",250,15,"Decrease the catch time by 250 milliseconds",13); 
-	addUpgrade("Money multiplier upgrade I",1500, "moneyMultiplier",0.25,0,"Gain 25% more money",null); 
-	addUpgrade("Money multiplier upgrade II",4000, "moneyMultiplier",0.30,3,"Gain 30% more money",15); 
-	addUpgrade("Money multiplier upgrade III",10000, "moneyMultiplier",0.35,6,"Gain 35% more money",16); 
-	addUpgrade("Money multiplier upgrade IV",25000, "moneyMultiplier",0.40,10,"Gain 40% more money",17); 
-	addUpgrade("Money multiplier upgrade V",50000, "moneyMultiplier",0.50,16,"Gain 50% more money",18); 
-	addUpgrade("Click multiplier upgrade I",100, "clickMultiplier",1,0,"Clicks do 100% more damage",null); 
-	addUpgrade("Click multiplier upgrade II",500, "clickMultiplier",1,2,"Clicks do 100% more damage",20); 
-	addUpgrade("Click multiplier upgrade III",1000, "clickMultiplier",1.5,5,"Clicks do 150% more damage",21); 
-	addUpgrade("Click multiplier upgrade IV",2500, "clickMultiplier",1.5,7,"Clicks do 150% more damage",22); 
-	addUpgrade("Click multiplier upgrade V",5000, "clickMultiplier",2,10,"Clicks do 200% more damage",23); 
+	addUpgrade("Pokeball upgrade I",0, "catchBonus",5,0,"New technology gives you a 5% bonus to catch rate",null); 
+	addUpgrade("Pokeball upgrade II",0, "catchBonus",5,3,"New technology gives you a 5% bonus to catch rate",0); 
+	addUpgrade("Pokeball upgrade III",0, "catchBonus",5,5,"New technology gives you a 5% bonus to catch rate",1); 
+	addUpgrade("Pokeball upgrade IV",0, "catchBonus",5,7,"New technology gives you a 5% bonus to catch rate",2); 
+	addUpgrade("Pokeball upgrade V",0, "catchBonus",10,10,"New technology gives you a 10% bonus to catch rate",3); 
+	addUpgrade("Experience upgrade I",0, "expMultiplier",0.10,0,"New technology gives you a 10% bonus to experience gain",null); 
+	addUpgrade("Experience upgrade II",0, "expMultiplier",0.10,1,"New technology gives you a 10% bonus to experience gain",5); 
+	addUpgrade("Experience upgrade III",0, "expMultiplier",0.10,4,"New technology gives you a 10% bonus to experience gain",6); 
+	addUpgrade("Experience upgrade IV",0, "expMultiplier",0.10,6,"New technology gives you a 10% bonus to experience gain",7); 
+	addUpgrade("Experience upgrade V",0, "expMultiplier",0.20,12,"New technology gives you a 20% bonus to experience gain",8); 
+	addUpgrade("Catch time upgrade I",0, "catchTime",250,0,"Decrease the catch time by 250 milliseconds",null); 
+	addUpgrade("Catch time upgrade II",0, "catchTime",250,2,"Decrease the catch time by 250 milliseconds",10); 
+	addUpgrade("Catch time upgrade III",0, "catchTime",250,5,"Decrease the catch time by 250 milliseconds",11); 
+	addUpgrade("Catch time upgrade IV",0, "catchTime",250,8,"Decrease the catch time by 250 milliseconds",12); 
+	addUpgrade("Catch time upgrade V",0, "catchTime",250,15,"Decrease the catch time by 250 milliseconds",13); 
+	addUpgrade("Money multiplier upgrade I",0, "moneyMultiplier",0.25,0,"Gain 25% more money",null); 
+	addUpgrade("Money multiplier upgrade II",0, "moneyMultiplier",0.30,3,"Gain 30% more money",15); 
+	addUpgrade("Money multiplier upgrade III",0, "moneyMultiplier",0.35,6,"Gain 35% more money",16); 
+	addUpgrade("Money multiplier upgrade IV",0, "moneyMultiplier",0.40,10,"Gain 40% more money",17); 
+	addUpgrade("Money multiplier upgrade V",0, "moneyMultiplier",0.50,16,"Gain 50% more money",18); 
+	addUpgrade("Click multiplier upgrade I",0, "clickMultiplier",1,0,"Clicks do 100% more damage",null); 
+	addUpgrade("Click multiplier upgrade II",0, "clickMultiplier",1,2,"Clicks do 100% more damage",20); 
+	addUpgrade("Click multiplier upgrade III",0, "clickMultiplier",1.5,5,"Clicks do 150% more damage",21); 
+	addUpgrade("Click multiplier upgrade IV",0, "clickMultiplier",1.5,7,"Clicks do 150% more damage",22); 
+	addUpgrade("Click multiplier upgrade V",0, "clickMultiplier",2,10,"Clicks do 200% more damage",23); 
 }
 
 
